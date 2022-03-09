8fdbd55f96a0f4b0b910862f726655ab0adb9547
mareis <mads.reistadbakk@mrfylke.no>
2022-03-09 14:05:48 +0100

    ,

---
+++ p5/empty-example/sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+    drawingContext.shadowOffsetX = 5;
+    drawingContext.shadowOffsetY = -5;
+    drawingContext.shadowBlur = 10;
+    drawingContext.shadowColor = "black";
+    background(100);
+    ellipse(width / 2, height / 2, 50, 50);
+}
+
+function draw() {
+    line(0, 0, 100, 100);
+    line(100, 0, 0, 100);
+}
