b4356c7f3865c091c83cf1aea2c05da6f671393d
mareis <mads.reistadbakk@mrfylke.no>
2022-06-01 13:18:47 +0200

    ,

--- 5_Domen/todo/main.js
+++ 5_Domen/todo/main.js
@@ -1,11 +1,11 @@
-let todoListe = [];
+let todoListe = ["Besøke Ona", "Bli fjellkonge", "Ta en kilosørret"];
+skrivUtTodo();
 
 function leggTilTodo() {
   let inputE = document.querySelector("#tekstboks");
 
   todoListe.push(inputE.value);
 
-  console.log(todoListe);
   inputE.value = "";
   skrivUtTodo();
 }
@@ -15,6 +15,11 @@ function skrivUtTodo() {
   for (let i = 0; i < todoListe.length; i++) {
     document.querySelector(
       "#gjøremål"
-    ).innerHTML += `<li> ${todoListe[i]}  </li>`;
+    ).innerHTML += `<li> <input type="checkbox"> ${todoListe[i]}  <button onclick="slettGjøremål(${i})">X</button> </li>`;
   }
+}
+
+function slettGjøremål(indeks) {
+  todoListe.splice(indeks, 1);
+  skrivUtTodo();
 }
